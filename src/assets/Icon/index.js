0aeaa41655edc67754721a30c076a6b285a78be5
dzurrotul <dzurrokw@gmail.com>
2021-03-27 11:31:48 +0700

    Laundry-App

---
+++ src/assets/Icon/index.js
@@ -0,0 +1,8 @@
+import IconAkun from './Akun.png'
+import IconAkunActive from './AkunActive.png'
+import IconHome from './Home.png'
+import IconHomeActive from './HomeActive.png'
+import IconPesanan from './Pesanan.png'
+import IconPesananActive from './PesananActive.png'
+
+export { IconAkun, IconAkunActive, IconHome, IconHomeActive, IconPesanan, IconPesananActive }
---
+++ src/router/index.js
@@ -0,0 +1,35 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+import { createStackNavigator } from '@react-navigation/stack';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Home from '../pages/Home';
+import Login from '../pages/Login';
+import Pesanan from '../pages/Pesanan';
+import Akun from '../pages/Akun';
+import BottomNavigator from '../components/BottomNavigator';
+
+const Stack = createStackNavigator();
+const Tab = createBottomTabNavigator();
+
+const MainApp = () => {
+  return (
+    <Tab.Navigator tabBar={props => <BottomNavigator {...props} />}>
+        <Tab.Screen name="Home" component={Home} />
+        <Tab.Screen name="Pesanan" component={Pesanan} />
+        <Tab.Screen name="Akun" component={Akun} />
+    </Tab.Navigator>
+  )
+}
+
+const Router = () => {
+    return (
+        <Stack.Navigator initialRouteName="MainApp">
+        <Stack.Screen name="Login" component={Login} options={{ headerShown: false }} />
+        <Stack.Screen name="MainApp" component={MainApp} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    )
+}
+
+export default Router
+
+const styles = StyleSheet.create({})
